683c55f3ad12efcc1c4ae64db4a8a1c6617c698b
unknown <michaelcordero88@gmail.com>
2018-05-01 14:22:37 -0700

    finishing project added country flags

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 
 let dataset2=[[20,45],[30,70],[100,50],[150,30],[70,35],[280,180],[15,30],[20,95]]
-let dataset;
+let nodeList;
+let linkList;
 let padding= 200;
-let width= 1500;
-let height= 800+ padding;
+let width= 1600;
+let height= 1000;
 let randomColor;
 
 
 // cylclist data
 document.addEventListener('DOMContentLoaded', function(){
-    fetch("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json")
+    fetch("https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json")
     .then((response)=>response.json())
     .then((data)=>{
-        console.log(data);
-        dataset= data;
-        randomColor= filterNames(dataset);
+     
+        nodeList= data.nodes;
+        linkList= data.links;
+        console.log(linkList)
+    
 
         setChart();
     })
@@ -23,132 +26,28 @@ document.addEventListener('DOMContentLoaded', function(){
     
 );
 
-function randomInt(min, max) {
-    return min + Math.floor(Math.random() * (Math.floor(max) - min));
-  }
 
 
-function filterNames(arr){
-    console.log(arr);
-let uniqueNames=[];
-let colors=[];
 
 
-    arr.map(function(d){
-let count=0;
-if (uniqueNames.indexOf(d.Name)===-1){
-    uniqueNames.push(d.Name);
-    colors.push("rgb("+randomInt(0,255)+","+randomInt(0,255)+","+randomInt(0,255)+")");
-}
 
-});
-
-return [uniqueNames,colors];
-}
-
-// maxtime will always be greatest overall time only need to subtract from max.
-function timeFormat(time, maxTime){
-let time1=time.split(":");
-let time2=maxTime.split(":");
-let min= time1[0];
-let sec= time1[1];
-let maxMin= time2[0];
-let maxSec= time2[1];
-let newTime=[];
-console.log(time1);
-newTime.push(maxMin-min);
-newTime.push(maxSec-sec);
-if (newTime[1]<0){
-    newTime[0] -= 1;
-    newTime[1]=60+newTime[1];
-    // newTime[1] will be negative
-    return newTime;
-}
-else {
-    return newTime;
-}
-
-
-}
-// input as seconds 
-function format(time){
-let min=0;
-let sec=0;
-
-    while(time>0){
-        if (time>60){
-            time-=60;
-            min++;
-            
-        }
-else{
-    sec=time;
-   time=0;
-}
-}
-
-let newTime=[min,sec].join(":");
-
-    return newTime;
-}
-
-
-console.log(console.log(format(2222)));
-
-///d3 3 phase enter update remove
 
 function setChart(){
-    
-let formatTime= d3.timeFormat("%M:%S");
-let parseTime= d3.timeParse("%M:%S")
-let rankMax=  d3.max(dataset,function(d){
-    
-    return d.Place;
-});
-let rankMin=d3.min(dataset,function(d){
-    return  d.Place;
-});
-let timeMax=  d3.max(dataset, function(d){
-   
-    return d.Seconds;
-});
-let timeMin=   d3.min(dataset, function(d){
-    return d.Seconds;
-});  
-
+ 
 
 let chart= d3.select("#chart")
 .append("svg")
-.attrs({
-    "width": width,
-    "height": height
-});
-let xscale= d3.scaleLinear();
-
-xscale.domain([timeMax-timeMax,timeMax-timeMin]);
-// can't use time format without converting to date...
-// since I cannot use date, 
-
-xscale.range([0,width-(padding*2)]);
-xscale.tickFormat(format);
-
-let yscale= d3.scaleLinear();
-yscale.domain([rankMin,rankMax]);
-yscale.range([0,height-(padding*2)]);
+.attr("width", width).attr("height",height);
 
 
-console.log(xscale.domain());
-
-
-
-chart.append("text").text("Doping In Professional Cycling")
+chart.append("text").text("Force-Directed-Graph")
 .attrs({
     "class": "title",
     "x": (width/2),
     "y": padding/2,
     "text-anchor": "middle"
 });
-chart.append("text").text("35 fastest times up Alpe d'Huez")
+chart.append("text").text("Countries that share borders")
 .attrs({
     "class": "subtitle",
     "x": (width/2),
@@ -156,50 +55,88 @@ chart.append("text").text("35 fastest times up Alpe d'Huez")
     "text-anchor": "middle"
 });
 
+let simulation= d3.forceSimulation(nodeList)
+.force("link", d3.forceLink(linkList).id(function(d){ 
+    
+    return d.index}))
+.force("charge", d3.forceManyBody())
+.force("center", d3.forceCenter(width/2,height/2));
 
-// if I map name data and store unique names in a list[] can create color chart for names.
-chart.selectAll("circle")
-.data(dataset)
+
+let links= chart.append("g").attr("class","links").attr("stroke","orange")
+.selectAll("line")
+.data(linkList)
+.enter()
+.append("line");
+console.log(links);
+
+
+let nodes= chart.append("g").attr("class","nodes")
+.selectAll("circle")
+.data(nodeList)
 .enter()
 .append("circle")
-.attrs({
- "cx": function(d){
-    return xscale(timeMax-d.Seconds)+ padding;
-},
- "cy": function(d){
-     return yscale(d.Place)+padding;
- },
- "r": function(d){
-    
-     return d.Seconds/300;
- },
- "fill": function(d){
-     let colorIndex= randomColor[0].indexOf(d.Name);
-     return randomColor[1][colorIndex];
- },
+.attr("r", function(d){
+    let count=0;
+    /// index is the country
+    console.log(d); 
+    for(i=0;i<linkList.length;i++){
+      // i is the index search
+        if(d.index===linkList[i].source.index){
 
-});
+            count++;
+        }
+        
+    }
+
+    
+
+return count*1.5;
+})
+.call(d3.drag()
+.on("start", dragStarted)
+.on("drag", dragged)
+.on("end", dragEnded)
+);
+
+
+
+
+
+// link drag forces//many body// returns to center.
+simulation.nodes(nodeList).on("tick",ticked);
+simulation.force("link").links(linkList);
+console.log(links);
 
 chart .selectAll("circle")
 .on("mouseenter",function(d){
 
     let x= d3.mouse(this)[0];
     let y= d3.mouse(this)[1];
+
+     
     if(!document.getElementById("tooltip")){
-        console.log(d.Name);
-   document.getElementById("data").innerHTML="<h3 id='description'>Name: " +d.Name+"<br /> Nationality: "+ d.Nationality+"<br />Year: "+ d.Year +"<br /> Time: "+d.Time+"<br />Allegations: "+ d.Doping;"</h3>"
-    chart.append("text")
-   .html(d.Name)
+      chart.append("image")
    .attrs({
-       "id": "tooltip",
        "x": x,
-       "y": y
+       "y": y,
+       "width": "50px",
+       "height": "40px",
+       "id": d.code,
+       "xlink:href": "./svg/"+d.code+".svg"
    });
+    chart.append("text")
+   .html(d.country)
+   .attr("id", "tooltip")
+   .attr("x", x)
+   .attr("y", y);
    }
+ 
+
 
 
 })
-.on("mousemove",function(){
+.on("mousemove",function(d){
  
   let x= d3.mouse(this)[0];
   let y= d3.mouse(this)[1];
@@ -207,63 +144,96 @@ chart .selectAll("circle")
  
   let tooltip= document.getElementById("tooltip");
   
-d3.select("#newline").attrs({
-    "x": x,
-    "y": y+20   
-});
+// d3.select("#newline").attrs({
+//     "x": x,
+//     "y": y+20   
+// });
       d3.select("#tooltip").attrs({
     "x": x,
     "y": y   
+   });
+   d3.select("#"+d.code).attrs({
+       "x": x,
+       "y": y-60
    }); 
    
 
 })
-.on("mouseleave",function(){
+.on("mouseleave",function(d){
     if(document.getElementById("tooltip")){
         
       d3.select("#tooltip").remove();
-      d3.select("#description").remove(); 
+      d3.select("#"+d.code).remove();
     }
    
 });
-xscale.range([width-(padding*2),0]);
-yscale.range([height-padding,padding]);
-let xAxis=d3.axisBottom(xscale).tickSizeInner([10]);
-let yAxis=d3.axisLeft(yscale).tickSizeInner([10]);
+
+function ticked(d){
+    links
+    .attr("x1", function(d){
+            return d.source.x;
+        })
+    .attr("y1", function(d){
+            return d.source.y;
+        })
+    .attr("x2", function(d){
+            return d.target.x;
+        })
+    .attr("y2", function(d){
+            return d.target.y;
+        });
+    nodes
+    .attr("cx", function(d){
+            return d.x;
+        })
+    .attr("cy", function(d){
+            return d.y;
+        });
+}
+function dragStarted(d){
+    if(!d3.event.active){ simulation.alphaTarget(0.3).restart();}
+d.fx= d.x;
+d.fy= d.y;
+}
+function dragged(d){
+d.fx= d3.event.x;
+d.fy= d3.event.y;
+}
+function dragEnded(d){
+    if(!d3.event.active){simulation.alphaTarget(0);}
+d.fx= null;
+d.fy= null;
+}
 
 
-chart.append("text").text("Place")
-.attr("text-anchor","middle")
-.attr("transform","translate("+padding/2+","+height/2+") rotate(-90)")
-.attr("class","label");
+
+
+// xscale.range([width-(padding*2),0]);
+// yscale.range([height-padding,padding]);
+// let xAxis=d3.axisBottom(xscale).tickSizeInner([10]);
+// let yAxis=d3.axisLeft(yscale).tickSizeInner([10]);
+
+
 
 
 // d3.select(".ylabel")
 //  .attr("transform","rotate(-90)");
 
 
- chart.append("g")
-.attr("class","yaxis")
-.attr("transform","translate("+padding+",0)")
-.call(yAxis);
+//  chart.append("g")
+// .attr("class","yaxis")
+// .attr("transform","translate("+padding+",0)")
+// .call(yAxis);
 
-chart.append("g")
-.attr("class","xaxis")
-.attr("transform","translate("+padding+","+(height-padding)+")")
-.call(xAxis);
-
-chart.append("text").text("Minutes Behind Fastest Time")
-.attr("text-anchor","middle")
-.attr("transform","translate("+width/2+","+padding*4.5+")")
-.attr("class","label");
+// chart.append("g")
+// .attr("class","xaxis")
+// .attr("transform","translate("+padding+","+(height-padding)+")")
+// .call(xAxis);
 
 
-d3.selectAll(".xaxis")
-.selectAll(".tick>text")
-.text(function(d){
-    console.log(format(d));
-    return format(d)
-});
+
+
+
 
 
 }
